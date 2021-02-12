d7de90f98aa609db84d2d4edb91100e3e1ef6117
ripzery <ripzery@gmail.com>
2021-02-12 16:11:46 +0700

    Fix migration and seed script

--- server/src/seeds/product.seed.ts
+++ server/src/seeds/product.seed.ts
@@ -4,6 +4,7 @@ import {Connection} from 'typeorm';
 
 const ProductSeed = [
   {
+    id: 1,
     title: 'Harry Potter and the Cursed Child',
     desc:
       'Harry Potter and the Cursed Child is a 2016 British two-part play written by Jack Thorne based on an original story by J. K. Rowling, John Tiffany, and Thorne. Previews of the play began at the Palace Theatre, London, on 7 June 2016, and it premiered on 30 July 2016.',
@@ -12,6 +13,7 @@ const ProductSeed = [
     updatedAt: `${new Date()}`,
   },
   {
+    id: 2,
     title: 'Harry Potter and the Deathly Hallows',
     desc:
       "The Minister for Magic Rufus Scrimgeour addresses the wizarding media, stating that the Ministry remains strong despite Lord Voldemort gaining power and the Death Eaters committing mass killings of Muggles and infiltrating the Ministry. Meanwhile, Harry Potter, Ron Weasley, and Hermione Granger resolve to complete the mission Albus Dumbledore gave Harry by hunting down and destroying Voldemort's Horcruxes. Severus Snape informs Voldemort of Harry's impending departure from Privet Drive.",
@@ -20,6 +22,7 @@ const ProductSeed = [
     updatedAt: `${new Date()}`,
   },
   {
+    id: 3,
     title: 'Harry Potter and the Goblet of Fire',
     desc:
       "Harry Potter awakens from a nightmare wherein a man named Frank Bryce is killed after overhearing Lord Voldemort conspiring with Peter Pettigrew and another man. While Harry attends the Quidditch World Cup match between Ireland and Bulgaria with the Weasleys and Hermione, Death Eaters terrorise the camp, and the man who appeared in Harry's dream summons the Dark Mark.      ",
@@ -28,6 +31,7 @@ const ProductSeed = [
     updatedAt: `${new Date()}`,
   },
   {
+    id: 4,
     title: "Harry Potter and the Sorcerer's Stone",
     desc:
       "The first novel in the Harry Potter series and Rowling's debut novel, it follows Harry Potter, a young wizard who discovers his magical heritage on his eleventh birthday, when he receives a letter of acceptance to Hogwarts School of Witchcraft and Wizardry.",
